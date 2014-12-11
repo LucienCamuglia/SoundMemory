58a4b3cf9a00daa0cc20f1512b62870ce1d512ca
LucienCamuglia <lucien.camuglia@gmail.com>
2014-12-11 16:44:08 +0100

    Play de la musique

--- Projet/JS/script.js
+++ Projet/JS/script.js
@@ -1,23 +1,45 @@
 /*
-Module : M152 
-Project : 18 - SoundMemory
-Page    : script.js
-InfoPage: Page contenant touts les scripts Javascript pour lire et arrêter une musique.
-Autor   : Camuglia Lucien, Perez Ivan
-Date    : 21.11.2014
-Version : 1.0 
-*/
+ Module : M152 
+ Project : 18 - SoundMemory
+ Page    : script.js
+ InfoPage: Page contenant touts les scripts Javascript pour lire et arrêter une musique.
+ Autor   : Camuglia Lucien, Perez Ivan
+ Date    : 21.11.2014
+ Version : 1.0 
+ */
 
 
+var audio ;
+$(function() {
+    $(".carte").click(function() {
+    	//predn l'ID de la carte.
+        var MusicId = this.id;
+        //prend le lien de la musique en relation avec la carte
+        var Lienaudio = $("#a" + MusicId).children()[0].getAttribute("src");
+        //crée l'objet audio
+        audio = new Audio(Lienaudio);
+        
+        console.log(audio.readyState);
+    
+audio.addEventListener("canplaythrough", function() { console.log("canplaythrough");audio.play();});    
+audio.load();
+        //démarre l'audio
+        
+audio.play();
 
+        //timer
+      /*  setInterval(function() {
+            console.log(audio.currentTime);
+            //test le temps de l'audio
+            if (audio.currentTime >= 29) {
+            	//met l'audio en pause
+                audio.pause();
+                //remet le temp a 0
+                audio.currentTime = 0;
+            }
+        }, 1000);*/
 
-$(function(){
-$( ".carte" ).click(function() {
- var MusicId = this.id;
-  var Lienaudio = $("#a"+MusicId).children()[0].getAttribute("src");
-  alert(Lienaudio);
-});
-
+    });
 
 
 
