4c79a77cbf7298b0641e683ff1dc53071bdeb73b
LucienCamuglia <patatezarbie@gmail.com>
2014-12-19 13:11:01 +0100

    corrections apres le merge

--- Projet/JS/script.js
+++ Projet/JS/script.js
@@ -1,5 +1,3 @@
-var nbEssais = 10 ;
-
 /*
  Module : M152 
  Project : 18 - SoundMemory
@@ -10,46 +8,55 @@ var nbEssais = 10 ;
  Version : 1.0 
  */
 
+var nbEssais = 10;
 
-var audio ;
+var audio;
 $(function() {
     $(".carte").click(function() {
-    	//predn l'ID de la carte.
- var MusicId = this.id;
+        //predn l'ID de la carte.
+        var MusicId = this.id;
         //prend le lien de la musique en relation avec la carte
         var Lienaudio = $("#a" + MusicId).children()[0].getAttribute("src");
-	if (nbEssais > 0)
-	{
-        //crée l'objet audio
-        audio = new Audio(Lienaudio);
-        
-        console.log(audio.readyState);
-    
-audio.addEventListener("canplaythrough", function() { console.log("canplaythrough");audio.play();});    
-audio.load();
-        //démarre l'audio
-        
-audio.play();
+        if (nbEssais > 0)
+        {
+            //crée l'objet audio
+            audio = new Audio(Lienaudio);
 
-        //timer
-      /*  setInterval(function() {
-            console.log(audio.currentTime);
-            //test le temps de l'audio
-            if (audio.currentTime >= 29) {
-            	//met l'audio en pause
-                audio.pause();
-                //remet le temp a 0
-                audio.currentTime = 0;
-            }
-        }, 1000);*/
-});
+            console.log(audio.readyState);
 
+            audio.addEventListener("canplaythrough", function() {
+                console.log("canplaythrough");
+                audio.play();
+            });
+            audio.load();
+            //démarre l'audio
+
+            audio.play();
+
+            //timer
+            /*  setInterval(function() {
+             console.log(audio.currentTime);
+             //test le temps de l'audio
+             if (audio.currentTime >= 29) {
+             //met l'audio en pause
+             audio.pause();
+             //remet le temp a 0
+             audio.currentTime = 0;
+             }
+             }, 1000);
+             });*/
+            nbEssais--;
+            AfficheEssais();
+
+        }else{
+         alert("vous avez Perdu !");   
+        };
     });
 
 
 
 })
 
-function AfficheEssais(){
-	$(".essais").text(nbEssais + " essais");
+function AfficheEssais() {
+    $(".essais").text(nbEssais + " essais");
 }
--- Projet/script/music.php
+++ Projet/script/music.php
@@ -1,15 +1,15 @@
 <?php
 
 /*
-Module : M152 
-Project : 18 - SoundMemory
-Page    : include.php
-InfoPage: Page contenant toutes les fonctions PHP relative au musique.
-Autor   : Camuglia Lucien, Perez Ivan
-Date    : 21.11.2014
-Version : 1.0 
+  Module : M152
+  Project : 18 - SoundMemory
+  Page    : include.php
+  InfoPage: Page contenant toutes les fonctions PHP relative au musique.
+  Autor   : Camuglia Lucien, Perez Ivan
+  Date    : 21.11.2014
+  Version : 1.0
 
-*/
+ */
 
 define('HOST', "localhost");
 define('DB_NAME', "soundmemory");
@@ -17,86 +17,74 @@ define('USER', 'root');
 define('PASSWORD', '');
 
 // DB Connection
-    /**
-     * Starts a Database connection
-     * @staticvar null $pdo
-     * @return $pdo the database
-     */
-function DBConnect()
-    {           
-        try
-        {
-            static $pdo = null;
-            if ($pdo === null)
-            {
-                $pdo = new PDO('mysql:host='.HOST.';dbname='.DB_NAME, USER, PASSWORD);
-                $pdo->exec('set character set utf8;');
-            }
-	}	
-        catch (Exception $e)
-	{
-            die('Erreur : '.$e->getMessage());
-	}
-	return $pdo;
+/**
+ * Starts a Database connection
+ * @staticvar null $pdo
+ * @return $pdo the database
+ */
+function DBConnect() {
+    try {
+        static $pdo = null;
+        if ($pdo === null) {
+            $pdo = new PDO('mysql:host=' . HOST . ';dbname=' . DB_NAME, USER, PASSWORD);
+            $pdo->exec('set character set utf8;');
+        }
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
     }
+    return $pdo;
+}
 
 /**
-     * Read all Medias from "Medias"
-     * @return array of assoc arrays - [0] => Array([Name] => [Value]), etc
-     */ 
-
+ * Read all Medias from "Medias"
+ * @return array of assoc arrays - [0] => Array([Name] => [Value]), etc
+ */
 require_once 'functions.php';
-function ReadMedias()
-    {
-        $pdo = DBConnect();
-        $query = 'SELECT * FROM Medias';
-        $st = $pdo->prepare($query);
-        $st->execute();
-        $result = $st->fetchAll(PDO::FETCH_ASSOC);
-        if (!empty($result))
-        {
-            return $result;
-        }
-        else
-        {
-            return FALSE;
-        }
+
+function ReadMedias() {
+    $pdo = DBConnect();
+    $query = 'SELECT * FROM Medias';
+    $st = $pdo->prepare($query);
+    $st->execute();
+    $result = $st->fetchAll(PDO::FETCH_ASSOC);
+    if (!empty($result)) {
+        return $result;
+    } else {
+        return FALSE;
     }
+}
+
 /**
-     * Read all Musics from "Medias"
-     * @return array of assoc arrays - [0] => Array([Name] => [Value]), etc
-     */    
-function ReadMusics()
-    {
-        $pdo = DBConnect();
-        $query = 'SELECT * FROM Medias WHERE MediaType = "Sound"';
-        $st = $pdo->prepare($query);
-        $st->execute();
-        $result = $st->fetchAll(PDO::FETCH_ASSOC);
-        if (!empty($result))
-        {
-            return $result;
-        }
-        else
-        {
-            return FALSE;
-        }
-    }    
-/**
-     * Reads a Media from "Medias"
-     * @param integer $Id - The id of the media you want
-     * @return Assoc Array - all the values from the record [Name] => [Value]
-     */
-function ReadMediaById($Id)
-    {
-        $pdo = DBConnect();
-        $query = 'SELECT * FROM Medias WHERE idMedia='.$Id;
-        $st = $pdo->prepare($query);
-        $st->execute();
-        $result = $st->fetch(PDO::FETCH_ASSOC);
-        return $result;     
+ * Read all Musics from "Medias"
+ * @return array of assoc arrays - [0] => Array([Name] => [Value]), etc
+ */
+function ReadMusics() {
+    $pdo = DBConnect();
+    $query = 'SELECT * FROM Medias WHERE MediaType = "Sound"';
+    $st = $pdo->prepare($query);
+    $st->execute();
+    $result = $st->fetchAll(PDO::FETCH_ASSOC);
+    if (!empty($result)) {
+        return $result;
+    } else {
+        return FALSE;
     }
-    
+}
+
+/**
+ * Reads a Media from "Medias"
+ * @param integer $Id - The id of the media you want
+ * @return Assoc Array - all the values from the record [Name] => [Value]
+ */
+function ReadMediaById($Id) {
+    $pdo = DBConnect();
+    $query = 'SELECT * FROM Medias WHERE idMedia=' . $Id;
+    $st = $pdo->prepare($query);
+    $st->execute();
+    $result = $st->fetch(PDO::FETCH_ASSOC);
+    return $result;
+}
+
 /**
  * Adds a media to "Medias"
  * @param enum $MediaType - An image or a music
@@ -104,20 +92,19 @@ function ReadMediaById($Id)
  * @param string $Path - the path of the media
  * @return integer - the id of the newly inserted media
  */
-function AddMedia($MediaType, $Name, $Path)
-    {
-        $pdo = DBConnect();
-        $query = 'INSERT INTO Medias (MediaType, Name, Path)
-                    VALUES(:MediaType,:Name,:Path)';				 
-        $st = $pdo->prepare($query);
-        $st->execute(array(
-            'MediaType' => $MediaType,
-            'Name' => $Name,
-            'Path' => $Path));
-	$id = $pdo->LastInsertID();
-	return $id;
-    }
- 
+function AddMedia($MediaType, $Name, $Path) {
+    $pdo = DBConnect();
+    $query = 'INSERT INTO Medias (MediaType, Name, Path)
+                    VALUES(:MediaType,:Name,:Path)';
+    $st = $pdo->prepare($query);
+    $st->execute(array(
+        'MediaType' => $MediaType,
+        'Name' => $Name,
+        'Path' => $Path));
+    $id = $pdo->LastInsertID();
+    return $id;
+}
+
 /**
  * Updates a media from "Medias"
  * @param integer $id - the id of the media
@@ -126,32 +113,30 @@ function AddMedia($MediaType, $Name, $Path)
  * @param string $Path - the path of the media
  * @return integer $id - the id of the modified Media
  */
- function UpdateMedia($id, $MediaType, $Name, $Path)
-    {
-        $pdo = DBConnect();
-        $query = 'Update Medias 
+function UpdateMedia($id, $MediaType, $Name, $Path) {
+    $pdo = DBConnect();
+    $query = 'Update Medias 
                 SET MediaType=:MediaType, Name=:Name, Path=:Path
-                WHERE idMedia=:id';				 
-        $st = $pdo->prepare($query);
-        $st->execute(array(
-            'id' => $id,
-            'MediaPath' => $MediaPath,
-            'Name' => $Name,
-            'Path' => $Path));
-	$id = $pdo->LastInsertID();
-	return $id;
-    }   
-    
- /**
-  * Deletes a media from "Medias"
-  * @param integer $id - the id of the media
-  */   
- function DeleteMedia($id)
-    {
-        $pdo = DBConnect();
-	$query = 'DELETE FROM Medias
+                WHERE idMedia=:id';
+    $st = $pdo->prepare($query);
+    $st->execute(array(
+        'id' => $id,
+        'MediaPath' => $MediaPath,
+        'Name' => $Name,
+        'Path' => $Path));
+    $id = $pdo->LastInsertID();
+    return $id;
+}
+
+/**
+ * Deletes a media from "Medias"
+ * @param integer $id - the id of the media
+ */
+function DeleteMedia($id) {
+    $pdo = DBConnect();
+    $query = 'DELETE FROM Medias
             WHERE idPMedia=:id';
-	$st = $pdo->prepare($query);
-	$st->execute(array(
-            'id' => $id));
-    }
+    $st = $pdo->prepare($query);
+    $st->execute(array(
+        'id' => $id));
+}
